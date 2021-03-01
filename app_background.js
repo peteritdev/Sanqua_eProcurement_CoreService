de07de7da6ff4cd15cd1e6fda9c6b0eee5c8eccd
Peter Susanto <peter.susanto@gracindo.co.id>
2021-03-01 15:23:03 +0700

    update add body limit to 50mb

--- app_background.js
+++ app_background.js
@@ -6,6 +6,9 @@ const cors = require('cors');
 const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
+app.use(bodyParser.json({
+  limit: '50mb'
+}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
