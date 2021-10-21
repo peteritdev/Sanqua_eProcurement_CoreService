24bf12b2dd7854c9749aa5912adb44c8ce83e999
Peter Susanto <peter.itdev@gmail.com>
2021-10-21 16:54:22 +0700

    update

--- server/repository/vendorcataloguerepository.js
+++ server/repository/vendorcataloguerepository.js
@@ -236,7 +236,7 @@ class VendorCatalogueRepository {
         var xData, xTotalRecord = [];
         var xSql = "";
         var xObjJsonWhere = {};
-        var xSqlWhere = " (1=1) ";
+        var xSqlWhere = " (1=1) vc.is_delete = 0 ";
         var xSqlOrderBy = "";
         var xSqlLimit = "";
 
