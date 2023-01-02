b6e9b897d35d45cce9e4b35a2fe63cc4ad4ffc21
Peter Susanto <peter.susanto@sanqua.co.id>
2023-01-02 11:56:16 +0700

    update

--- server/routes/export.js
+++ server/routes/export.js
@@ -13,7 +13,7 @@ module.exports = (app) => {
 		res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
 		res.header(
 			'Access-Control-Allow-Headers',
-			'Origin, X-Requested-With, Content-Type, Accept, x-token, x-method, x-device'
+			'Origin, X-Requested-With, Content-Type, Accept, x-token, x-method, x-device, x-device-id'
 		);
 		next();
 	});
