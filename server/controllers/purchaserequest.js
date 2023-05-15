ca84fe17942b7b714b6c65caedab97e1c97ac7e4
Peter Susanto <peter.susanto@sanqua.co.id>
2023-05-15 13:32:53 +0700

    update

--- server/controllers/purchaserequest.js
+++ server/controllers/purchaserequest.js
@@ -84,8 +84,8 @@ async function purchaseRequest_List(req, res) {
 					}
 				}
 
-				req.body.logged_company_id = oAuthResult.token_data.result_verify.employee_info.company.id;
-				req.body.logged_company_name = oAuthResult.token_data.result_verify.employee_info.company.name;
+				req.query.logged_company_id = oAuthResult.token_data.result_verify.employee_info.company.id;
+				req.query.logged_company_name = oAuthResult.token_data.result_verify.employee_info.company.name;
 				req.query.method = req.headers['x-method'];
 				req.query.token = req.headers['x-token'];
 				joResult = await _serviceInstance.list(req.query);
