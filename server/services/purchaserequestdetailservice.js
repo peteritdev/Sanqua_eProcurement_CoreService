84694b34b6525dac58590acb7cd5651f3c31ccbe
Peter Susanto <peter.susanto@sanqua.co.id>
2023-11-14 13:36:55 +0700

    update

--- server/services/purchaserequestdetailservice.js
+++ server/services/purchaserequestdetailservice.js
@@ -141,6 +141,7 @@ class PurchaseRequestDetailService {
 
 					xAct = 'update';
 				} else {
+					console.log(`>>> pParam CEK CEK CEK : ${JSON.stringify(pParam)}`);
 					if (pParam.hasOwnProperty('product_id')) {
 						if (pParam.product_id != '' && pParam.product_id != null) {
 							// Get Product detail by Id
