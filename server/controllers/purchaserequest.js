48395f8bf5f98b68ed2d608f174a0cd4f6b736f1
glitchfer <arif.0496.rahman@gmail.com>
2024-08-15 11:59:48 +0700

    add api cancel item fpb

--- server/controllers/purchaserequest.js
+++ server/controllers/purchaserequest.js
@@ -44,7 +44,8 @@ module.exports = {
 	purchaseRequestDetail_SendNotificationEqualizationOdoo,
 
 	purchaseRequestDetail_RefreshItem,
-	purchaseRequest_FetchMatrix
+	purchaseRequest_FetchMatrix,
+	purchaseRequestDetail_CancelItem
 };
 
 async function purchaseRequest_List(req, res) {
@@ -1094,6 +1095,40 @@ async function purchaseRequest_FetchMatrix(req, res) {
 				req.body.notification_via_wa = oAuthResult.token_data.result_verify.notification_via_wa;
 				req.body.notification_via_telegram = oAuthResult.token_data.result_verify.notification_via_telegram;
 				joResult = await _serviceInstance.fetchMatrixFPB(req.body);
+				joResult = JSON.stringify(joResult);
+			}
+		} else {
+			joResult = JSON.stringify(oAuthResult);
+		}
+	} else {
+		joResult = JSON.stringify(oAuthResult);
+	}
+
+	res.setHeader('Content-Type', 'application/json');
+	res.status(200).send(joResult);
+}
+
+async function purchaseRequestDetail_CancelItem(req, res) {
+	var joResult;
+	var oAuthResult = await _oAuthServiceInstance.verifyToken(req.headers['x-token'], req.headers['x-method']);
+
+	if (oAuthResult.status_code == '00') {
+		if (oAuthResult.token_data.status_code == '00') {
+			// Validate first
+			var errors = validationResult(req).array();
+
+			if (errors.length != 0) {
+				joResult = JSON.stringify({
+					status_code: '-99',
+					status_msg: 'Parameter value has problem',
+					error_msg: errors
+				});
+			} else {
+				req.body.logged_user_id = oAuthResult.token_data.result_verify.id;
+				req.body.logged_user_name = oAuthResult.token_data.result_verify.name;
+				req.body.token = req.headers['x-token'];
+				req.body.method = req.headers['x-method'];
+				joResult = await _serviceDetailInstance.cancelItem(req.body);
 				joResult = JSON.stringify(joResult);
 			}
 		} else {
