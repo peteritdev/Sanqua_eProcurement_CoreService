12961b65e8e2bd0a713fc6bb26507823280f1a08
glitchfer <arif.0496.rahman@gmail.com>
2024-10-29 15:37:25 +0700

    up

--- server/services/exportservice.js
+++ server/services/exportservice.js
@@ -565,7 +565,7 @@ class ExportService {
 						xOptions = {
 							height: '212mm',
 							// height: '148.5mm',
-							width: '155mm',
+							width: '153mm',
 							borders: '0.3cm'
 						};
 
