ce61ddc8770ab507e3a2f0f622c7305102cc15b9
glitchfer <arif.0496.rahman@gmail.com>
2025-07-26 06:49:14 +0700

    hide puppeterr

--- server/controllers/export.js
+++ server/controllers/export.js
@@ -10,7 +10,7 @@ const env         = process.env.NODE_ENV || 'localhost';
 const config      = require(__dirname + '/../config/config.json')[env];
 const { check, validationResult } = require('express-validator');
 
-module.exports = { generateFPB, generateRAB, generateRAB_V2 }
+module.exports = { generateFPB, generateRAB }
 
 async function generateFPB( req, res ){
     var joResult = {};
@@ -54,25 +54,24 @@ async function generateRAB( req, res ){
         res.status(200).send(joResult);
     }
 }
+// async function generateRAB_V2( req, res ){
+//     var joResult = {};
+//     var oAuthResult = await oAuthServiceInstance.verifyToken( req.headers['x-token'], req.headers['x-method'] );
 
-async function generateRAB_V2( req, res ){
-    var joResult = {};
-    var oAuthResult = await oAuthServiceInstance.verifyToken( req.headers['x-token'], req.headers['x-method'] );
+//     console.log(">>> Here");
 
-    console.log(">>> Here");
-
-    if( oAuthResult.status_code == "00" ){
-        if( oAuthResult.token_data.status_code == "00" ){
-            // await _serviceInstance.generateRAB(req.params.id, req.headers['x-method'], req.headers['x-token'], res);
-            await _serviceInstance.generateRAB_Puppeteer(req.params.id, req.headers['x-method'], req.headers['x-token'], res);
-        }else{
-            joResult = JSON.stringify(oAuthResult);
-            res.setHeader('Content-Type','application/json');
-            res.status(200).send(joResult);
-        }
-    }else{
-        joResult = JSON.stringify(oAuthResult);
-        res.setHeader('Content-Type','application/json');
-        res.status(200).send(joResult);
-    }
-}
+//     if( oAuthResult.status_code == "00" ){
+//         if( oAuthResult.token_data.status_code == "00" ){
+//             // await _serviceInstance.generateRAB(req.params.id, req.headers['x-method'], req.headers['x-token'], res);
+//             await _serviceInstance.generateRAB_Puppeteer(req.params.id, req.headers['x-method'], req.headers['x-token'], res);
+//         }else{
+//             joResult = JSON.stringify(oAuthResult);
+//             res.setHeader('Content-Type','application/json');
+//             res.status(200).send(joResult);
+//         }
+//     }else{
+//         joResult = JSON.stringify(oAuthResult);
+//         res.setHeader('Content-Type','application/json');
+//         res.status(200).send(joResult);
+//     }
+// }
