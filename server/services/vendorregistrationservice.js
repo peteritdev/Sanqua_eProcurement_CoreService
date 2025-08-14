bb87ea37799a38c4540d3bc6cc5ff0fe4a968837
glitchfer <arif.0496.rahman@gmail.com>
2025-08-14 09:36:07 +0700

    fix file vendor_doc

--- server/services/vendorregistrationservice.js
+++ server/services/vendorregistrationservice.js
@@ -192,7 +192,7 @@ class VendorRegistrationService {
 					if (xDocumentFile != null) {
 						for (let i = 0; i < xDocumentFile.length; i++) {
 							if (xDocumentFile[i].file != null) {
-								xDetail.vendor_document[i].file = `${config.imagePathESanQua}/eprocurement/vendor-regist/${xDocumentFile[i].file}`
+								xDetail.vendor_document[i].file = `${config.imagePathESanQua}/eprocurement/vendor_regist/${xDocumentFile[i].file}`
 							}
 						}
 					}
