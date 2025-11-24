84f7fedf6b604b72b8da964c8f6dd1d608f14134
glitchfer <arif.0496.rahman@gmail.com>
2025-11-24 11:49:43 +0700

    up

--- server/controllers/purchaserequest.js
+++ server/controllers/purchaserequest.js
@@ -74,7 +74,7 @@ async function purchaseRequest_List(req, res) {
 					(el) => el.application.id === config.applicationId || el.application.id === 1
 				);
 
-				req.query.logged_is_admin = xLevel.is_admin;
+				req.query.logged_is_admin = xLevel != undefined ? xLevel.is_admin : 0;
 				req.query.user_id = oAuthResult.token_data.result_verify.id;
 				if (oAuthResult.token_data.result_verify.employee_info.department.hasOwnProperty('unit')) {
 					if (oAuthResult.token_data.result_verify.employee_info.department.unit != null) {
