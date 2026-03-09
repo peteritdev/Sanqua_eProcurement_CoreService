a25fad7c3d3d688d5efebabb1d5325b2d22801f2
glitchfer <arif.0496.rahman@gmail.com>
2026-03-09 13:55:59 +0700

    fix error modul pjca on payreq

--- server/models/paymentrequest.js
+++ server/models/paymentrequest.js
@@ -74,6 +74,10 @@ module.exports = (sequelize, DataTypes) => {
 			foreignKey: 'purchase_request_id',
 			as: 'purchase_request'
 		});
+		PaymentRequest.hasMany(models.tr_pjcas, {
+			foreignKey: 'payment_request_id',
+			as: 'pjca'
+		});
 	};
 
 	return PaymentRequest;
